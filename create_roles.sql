d62d5c5c0905b13339bd10851c051e7e6003abf1
Kreiser <kreiser2015@mail.ru>
2026-03-15 19:11:34 +0300

    Update create_roles.sql
    Улучшение.

--- create_roles.sql
+++ create_roles.sql
@@ -10,7 +10,7 @@ BEGIN
     END IF;
     
     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'worker') THEN
-		CREATE ROLE worker;
+        CREATE ROLE worker;
     END IF;
     
     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'executive') THEN
@@ -25,14 +25,10 @@ BEGIN
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO administrator;
     GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO administrator;
     
-    GRANT SELECT, INSERT, UPDATE ON TABLE Companies TO company;
-    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Hardware TO company;
-    GRANT SELECT, INSERT, UPDATE ON TABLE Requests TO company;
-    GRANT USAGE ON SEQUENCE hardware_id_seq TO company;
-    GRANT USAGE ON SEQUENCE requests_id_seq TO company;
-    
-    GRANT SELECT, INSERT, UPDATE ON TABLE Requests TO worker;
-    GRANT USAGE ON SEQUENCE requests_id_seq TO worker;
+    GRANT INSERT ON TABLE Hardware TO company;
+    GRANT INSERT ON TABLE Requests TO company;
+
+    GRANT INSERT ON TABLE Requests TO worker;
     
     GRANT SELECT ON TABLE Tasks TO executive;
     GRANT SELECT ON TABLE Requests TO executive;
@@ -40,11 +36,8 @@ BEGIN
     GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE Tasks TO distributor;
     GRANT SELECT ON TABLE Requests TO distributor;
     GRANT SELECT ON TABLE Staff TO distributor;
-    GRANT USAGE ON SEQUENCE tasks_taskid_seq TO distributor;
 
-    GRANT SELECT ON TABLE Companies TO company, worker, executive, distributor;
-    GRANT SELECT ON TABLE Contracts TO company, worker, executive, distributor;
-    GRANT SELECT ON TABLE Hardware TO company, worker, executive, distributor;
-    GRANT SELECT ON TABLE Staff TO company, worker, executive, distributor;
+    GRANT SELECT ON TABLE Companies TO distributor;
+    GRANT SELECT ON TABLE Contracts TO distributor;
 END;
 $$ LANGUAGE plpgsql;
