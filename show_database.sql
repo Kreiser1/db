5f1c55b555d862856ea3d3d3ce3a2080d16e98c2
Kreiser <kreiser2015@mail.ru>
2026-03-17 18:10:53 +0300

    Update show_database.sql

--- show_database.sql
+++ show_database.sql
@@ -1,28 +1,21 @@
-CREATE OR REPLACE PROCEDURE show_database()
-AS $$
-BEGIN
-    SELECT 
-        c.shortName AS "Краткое наименование",
-        c.fullName AS "Полное наименование",
-        c.representativePosition AS "Должность представителя",
-        c.contactPhone AS "Телефон",
-        c.physicalAddress AS "Физический адрес",
-        c.legalAddress AS "Юридический адрес",
-        c.contractId AS "Номер контракта"
-    FROM Companies c
-    ORDER BY c.shortName ASC;
-    
-    SELECT 
-        s.contractId AS "Контракт",
-        s.lastName AS "Фамилия",
-        s.firstName AS "Имя",
-        s.patronymic AS "Отчество",
-        s.position AS "Должность",
-        s.department AS "Отдел",
-        s.login AS "Логин"
-    FROM Staff s
-    ORDER BY s.login ASC;
-END;
-$$ LANGUAGE plpgsql;
+SELECT 
+    c.shortName AS "Краткое наименование",
+    c.fullName AS "Полное наименование",
+    c.representativePosition AS "Должность представителя",
+    c.contactPhone AS "Телефон",
+    c.physicalAddress AS "Физический адрес",
+    c.legalAddress AS "Юридический адрес",
+    c.contractId AS "Номер контракта"
+FROM Companies c
+ORDER BY c.shortName ASC;
 
-call show_database();
+SELECT 
+    s.contractId AS "Контракт",
+    s.lastName AS "Фамилия",
+    s.firstName AS "Имя",
+    s.patronymic AS "Отчество",
+    s.position AS "Должность",
+    s.department AS "Отдел",
+    s.login AS "Логин"
+FROM Staff s
+ORDER BY s.login ASC;
