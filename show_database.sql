03e1a8e76915b1f359e847ff65e791836c885afa
Kreiser <kreiser2015@mail.ru>
2026-03-17 18:16:34 +0300

    Update show_database.sql

--- show_database.sql
+++ show_database.sql
@@ -1,21 +1,22 @@
-SELECT 
-    c.shortName AS "Краткое наименование",
-    c.fullName AS "Полное наименование",
-    c.representativePosition AS "Должность представителя",
-    c.contactPhone AS "Телефон",
-    c.physicalAddress AS "Физический адрес",
-    c.legalAddress AS "Юридический адрес",
-    c.contractId AS "Номер контракта"
-FROM Companies c
-ORDER BY c.shortName ASC;
+SELECT 'Организация: ' || fullName || ' (' || shortName || ')' as info FROM Companies
+UNION ALL
+SELECT 'Юр. адрес: ' || legalAddress || ', физ. адрес: ' || physicalAddress || ', тел: ' || contactPhone FROM Companies;
 
-SELECT 
-    s.contractId AS "Контракт",
-    s.lastName AS "Фамилия",
-    s.firstName AS "Имя",
-    s.patronymic AS "Отчество",
-    s.position AS "Должность",
-    s.department AS "Отдел",
-    s.login AS "Логин"
-FROM Staff s
-ORDER BY s.login ASC;
+SELECT 'Договор: ' || c.contractId || ', ' || c.shortName FROM Companies c;
+
+SELECT '  Отдел: ' || department || ', ' || position || ' - ' || 
+       lastName || ' ' || firstName || ' (' || login || ', ' || password || ')'
+FROM Staff WHERE contractId IS NOT NULL;
+
+SELECT 'Исполнитель: ' || lastName || ' ' || firstName || ' - ' || 
+       position || ' (' || login || ', ' || password || ')'
+FROM Staff WHERE contractId IS NULL;
+
+SELECT 'Заявка: ' || id || ', от ' || creationTime || ', договор: ' || contractId || ', заявитель: ' || responsibleLogin
+FROM Requests;
+
+SELECT '  Оборудование: ' || h.description 
+FROM Requests r JOIN Hardware h ON r.hardwareId = h.id;
+
+SELECT '  Задача: ' || t.task || ' (' || s.lastName || ', ' || t.executorLogin || ')'
+FROM Tasks t JOIN Staff s ON t.executorLogin = s.login;
