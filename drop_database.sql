0ca025a693ff2fe3c7672bae32204b009aaf04b5
Kreiser <kreiser2015@mail.ru>
2026-04-27 19:42:49 +0300

    Update drop_database.sql

--- drop_database.sql
+++ drop_database.sql
@@ -1,21 +1,13 @@
-CREATE OR REPLACE PROCEDURE drop_database()
-AS $$
-BEGIN
-    SET CONSTRAINTS ALL DEFERRED;
+SET CONSTRAINTS ALL DEFERRED;
     
-    DELETE FROM Tasks;
-    DELETE FROM Requests;
-    DELETE FROM Staff;
-    DELETE FROM Hardware;
-    DELETE FROM Contracts;
-    DELETE FROM Companies;
-    
-    PERFORM setval('hardware_id_seq', 1, false);
-    PERFORM setval('requests_id_seq', 1, false);
-    PERFORM setval('tasks_taskid_seq', 1, false);
-    
-    SET CONSTRAINTS ALL IMMEDIATE;
-END;
-$$ LANGUAGE plpgsql;
+DELETE FROM Tasks;
+DELETE FROM Requests;
+DELETE FROM Staff;
+DELETE FROM Hardware;
+DELETE FROM Contracts;
+DELETE FROM Companies;
+DELETE FROM Transport;
 
-call drop_database();
+PERFORM setval('hardware_id_seq', 1, false);
+
+SET CONSTRAINTS ALL IMMEDIATE;
